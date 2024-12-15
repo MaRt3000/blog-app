56d6ddb35f741068b8f59b71639aa512912715e3
MaRt <ogbemart@gmail.com>
2024-12-15 14:44:56 +0100

    made changes on the tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-   content: [
+  content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-   plugins: [
-    require('@tailwindcss/typography'),
+  plugins: [
+    typography,
   ],
 }
-
